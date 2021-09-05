fc7bb27188e5a4a06bcde08cc3e1d57d26f50b24
Sreeram <sreerambeece@gmail.com>
2021-09-05 11:11:22 +0530

    Updated classes

---
+++ src/main/java/javapractice/MyFirstProgram.java
@@ -0,0 +1,13 @@
+package javapractice;
+
+public class MyFirstProgram {
+	static int var =10;
+	public static void main(String[] args) {
+
+		int varInt= 20;
+		String name ="ram";
+		System.out.println("My First Java Program");
+		System.out.format("Hello %d %s %d", varInt, name, var);
+	}
+
+}
