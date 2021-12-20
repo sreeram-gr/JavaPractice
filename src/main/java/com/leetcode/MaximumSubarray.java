6d1116cb070c25679c503a6cfdea53597fd2d781
Sreeram <sreerambeece@gmail.com>
2021-12-20 18:17:16 +0530

    Added MaximumSubarray

---
+++ src/main/java/com/leetcode/MaximumSubarray.java
@@ -0,0 +1,23 @@
+package com.leetcode;
+
+public class MaximumSubarray {
+
+	public static void main(String[] args) {
+		System.out.println(maxSubArray(new int[] {-2,1}));
+
+	}
+	public static int maxSubArray(int[] nums) {
+		int maxSum=nums[0], maxValue=0;
+		for(int i=0;i<nums.length;i++) {
+			maxValue=maxValue+nums[i];
+			if(maxSum<maxValue) {
+				maxSum=maxValue;
+			}
+			if(maxValue<0){
+				maxValue=0;
+			}
+		}
+		return maxSum;
+    }
+
+}
